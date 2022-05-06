08f15f8ae65ec29d38d229e05b4cd9560ebda06e
yuu <meny1122men@gmail.com>
2022-05-06 19:47:49 +0900

    エラーメッセージの表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,22 +2,26 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
 
-    @book = Book.new(book_params)
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to books_path
+      flash[:notice] = "かんぺき"
+      redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy
